2ad311d88274014b94f7b31509e866414520252f
dain391 <0901ekdls@gmail.com>
2025-04-25 21:25:14 +0900

    Kiosk Lv2 complete

---
+++ src/MenuItem.java
@@ -0,0 +1,30 @@
+public class MenuItem {
+    private String name; // 메뉴 이름
+    private double price; // 가격
+    private String info; // 설명
+
+    // 생성자
+    public MenuItem(String name, double price, String info) {
+        this.name = name;
+        this.price = price;
+        this.info = info;
+    }
+
+    // getter
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    // 메뉴 정보 출력
+    public void printMenu() {
+        System.out.println(name + " | w " + price + " | " + info);
+    }
+}
